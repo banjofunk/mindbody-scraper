b285cd24d19867b00a3469c92e2f1823ae41744d
banjofunk <joshsfunkybanjo@gmail.com>
2019-05-17 16:02:19 -0700

    resources table

--- functions/getResources.js
+++ functions/getResources.js
@@ -14,7 +14,20 @@ exports.handler = async (event, context) => {
     options: {},
     respType: 'json'
   }
-  const resources = await mbFetch(fetchParams)
-  console.log('resources', resources)
-  return resources
+  const { Resources, Locations, ServiceCategoryVisitTypes } = await mbFetch(fetchParams)
+  const locations = Locations.map(l => ({...l, id: l.Id}))
+  const resources = Resources.map(r => ({...r, id: r.ResourceId}))
+  const visits = ServiceCategoryVisitTypes.map(v => ({...v, id: v.VisitTypeId}))
+  const resourceLengths = { locations: locations.length, resources: resources.length, visits: visits.length }
+  await logger(session, `Processing Resources: ${JSON.stringify(resourceLengths)}`)
+  await Promise.all(resources.map(resource => 
+    writeToDynamo('resourceId', resource, 'ResourcesTable')
+  ))
+  await Promise.all(locations.map(location => 
+    writeToDynamo('locationId', location, 'LocationsTable')
+  ))
+  await Promise.all(visits.map(visit => 
+    writeToDynamo('serviceCategoryVisitTypeId', visit, 'ServiceCategoryVisitTypesTable')
+  ))
+  return Promise.resolve()
 }
